5f408ad933933fe99cc29c60a6a21e082f7bbfb9
Ale421-rad <ale42570@gmail.com>
2025-06-29 12:27:16 +0900

    Update script.js

--- script.js
+++ script.js
@@ -195,7 +195,7 @@ confirmButton.addEventListener('click', function() {
  // Transférer les données vers la page fiche personnage
  document.getElementById("character-name").textContent = document.getElementById("confirm-name").textContent;
  document.getElementById("character-image").src = document.getElementById("confirm-image").src;
-document.getElementById("character-special").textContent = document.getElementById("confirm-special").textContent;
+ document.getElementById("character-special").textContent = document.getElementById("confirm-special").textContent;
  document.getElementById("character-special").innerHTML = `${characterSpecial1}<br>${characterSpecial2}`;
 
     resetCharacterStats(); // Reset stats when confirming
